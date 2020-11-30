7bcb4cf3fa88f751dceeb8f089c0f2a0f27ed09f
Bruno Brasolin <brunobrasolinc@gmail.com>
2020-11-30 20:23:14 -0300

    Criando metodos de deletar e inserir

--- P1/src/java/br/edu/fatecpg/poo/Disciplina.java
+++ P1/src/java/br/edu/fatecpg/poo/Disciplina.java
@@ -6,6 +6,7 @@
 package br.edu.fatecpg.poo;
 
 import java.util.ArrayList;
+import java.sql.*;
 
 /**
  *
@@ -13,12 +14,14 @@ import java.util.ArrayList;
  */
 public class Disciplina {
 
+    private long rowId;
     private String nome;
     private String ementa;
     private int ciclo;
     private double nota;
 
-    public Disciplina(String nome, String ementa, int ciclo) {
+    public Disciplina(long rowId, String nome, String ementa, int ciclo, double nota) {
+        this.rowId = rowId;
         this.nome = nome;
         this.ementa = ementa;
         if (ciclo < 1) {
@@ -27,17 +30,63 @@ public class Disciplina {
             ciclo = 6;
         }
         this.ciclo = ciclo;
+        this.nota = nota;
     }
 
-    public Disciplina getList(int i) {
-        ArrayList<Disciplina> d = new ArrayList<Disciplina>();
-        d.add(new Disciplina("Banco de Dados", "Ementa BD", 4));
-        d.add(new Disciplina("Engenharia de Software III", "Ementa ES", 4));
-        d.add(new Disciplina("Programação Orientada a Objetos", "Ementa POO", 4));
-        d.add(new Disciplina("Linguagem de Programação IV", "Ementa LP", 4));
-        d.add(new Disciplina("Sistemas Operacionais II", "Ementa SO", 4));
-        d.add(new Disciplina("Metodologia da Pesquisa Científico-Tecnológica", "Ementa MET", 4));
-        return d.get(i);
+    public static ArrayList<Disciplina> getList() {
+        ArrayList<Disciplina> list = new ArrayList<>();
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet result = null;
+
+        try {
+            connection = DBListener.getConnection();
+            statement = connection.createStatement();
+            result = statement.executeQuery("SELET rowid, * FROM disciplinas");
+            while (result.next()) {
+                list.add(new Disciplina(
+                        result.getLong("rowid"),
+                        result.getString("nome"),
+                        result.getString("ementa"),
+                        result.getInt("ciclo"),
+                        result.getDouble("nota")
+                ));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return list;
+    }
+
+    public static void insert(String nome, String ementa, int ciclo, double nota) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+            connection = DBListener.getConnection();
+            statement = connection.prepareStatement("INSERT INTO disciplinas VALUES(?,?,?,?)");
+            statement.setString(1, nome);
+            statement.setString(2, ementa);
+            statement.setInt(3, ciclo);
+            statement.setDouble(4, nota);
+            statement.execute();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void delete(long rowid) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+            connection = DBListener.getConnection();
+            statement = connection.prepareStatement("DELETE FROM disciplinas WHERE rowid=?");
+            statement.execute();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public String getNome() {
@@ -62,7 +111,6 @@ public class Disciplina {
 
     public void setEmenta(String ementa) {
         this.ementa = ementa;
-
     }
 
     public void setCiclo(int ciclo) {
@@ -76,5 +124,22 @@ public class Disciplina {
 
     public void setNota(double nota) {
         this.nota = nota;
+    }
+
+    public void setRowId(long rowId) {
+        this.rowId = rowId;
+    }
+
+    public long getRowId() {
+        return rowId;
+    }
+    
+    public static String getCreateTableStatement() {
+        return "CREATE TABLE IF NOT EXISTS disciplinas("
+                + "nome VARCHAR(255) UNIQUE NOT NULL,"
+                + "ementa VARCHAR(255) NOT NULL,"
+                + "ciclo INT NOT NULL,"
+                + "nota DOUBLE NOT NULL"
+                + ")";
     }
 }
